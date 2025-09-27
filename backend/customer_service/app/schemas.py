e13fee9dd740536993b5c97315f3a2a5e6cd270c
pasco-1514 <pascobett1514@gmail.com>
2025-09-27 21:44:36 +1000

    testing stage 1 and 2

--- backend/customer_service/app/schemas.py
+++ backend/customer_service/app/schemas.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
-# // Testing CI
+# // Testing CI 1
 
 
 class CustomerBase(BaseModel):
